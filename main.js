b097825174643cc9469ce1049962d5b4a9fdc882
PPIC <69889835+PPICWG@users.noreply.github.com>
2025-08-02 23:46:33 +0700

    main.js

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
  
         const CONFIG = {
-            SHEET_ID: '18ga4stLBiKWujBryPQHUHrUzgYjprFJEKvUWSd3Zcks', 
-            SHEET_NAME: 'WKB', 
-            API_KEY: 'AIzaSyBBY9oqeEjjpVnIXOhdkhR6xuTsCr5gYU8', 
+            
             ITEMS_PER_PAGE: 10,
             REFRESH_INTERVAL: 600000, 
             TARGET_DATE_CELL: 'J1'
